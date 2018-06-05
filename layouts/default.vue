cb5f57e875e5104ca737336c6a17e96295b15e11
YbCloud <abso8787@naver.com>
2018-06-05 12:01:52 +0900

    0605

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,29 @@
 <template>
-    <div>
+  <div>
     <nav class="navbar">
       <div class="navbar-brand">
         <nuxt-link to="/" class="navbar-item">
-            <figure class="image is-32x32">
-              <img src="~static/dogfoot.jpg" alt="">
-           </figure>
+          <figure class="image is-32x32">
+            <img src="~static/dogfoot.jpg" alt="">
+          </figure>
         </nuxt-link>
-      
+  
       </div>
       <div class="navbar-menu is-active">
         <div class="navbar-end">
-          <nuxt-link to="/stat" class="navbar-item">통계</nuxt-link>
-          <nuxt-link to="/about" class="navbar-item">소개</nuxt-link>
+          <nuxt-link to="/" class="navbar-item">
+            <a class="button is-danger">소개</a>
+         </nuxt-link>
+          <nuxt-link to="/tech" class="navbar-item">기술</nuxt-link>
+          <nuxt-link to="/history" class="navbar-item">이력</nuxt-link>
         </div>
       </div>
     </nav>
     <nuxt/>
-
+  
+    <hr>
+  
+  
     <footer class="footer has-text-centered">
       <div>2018년 목원대학교 김용빈의 포트폴리오 웹사이트입니다.</div>
     </footer>
@@ -25,4 +31,5 @@
 </template>
 
 <style>
+  
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,56 @@
 <template>
   <section class="container">
-    <div>
-      1360007 김용빈
+  
+    <hr>
+  
+    <div class="box">
+      <h1 class="title is-4">안녕하세요 김용빈을 소개하는 웹사이트입니다.</h1>
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+          </figure>
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>이름 :</strong> <small> 김용빈</small> 
+              <br>
+              <strong>나이 :</strong> <small> 25</small> 
+              <br> 
+              <strong>학교 :</strong> <small> 목원대학교</small> 
+              <br>  
+            </p>
+          </div>
+          <nav class="level is-mobile">
+            <div class="level-left">
+              <a class="level-item" aria-label="reply">
+                <span class="icon is-small">
+                <i class="fas fa-reply" aria-hidden="true"></i>
+              </span>
+              </a>
+              <a class="level-item" aria-label="retweet">
+                <span class="icon is-small">
+                <i class="fas fa-retweet" aria-hidden="true"></i>
+              </span>
+              </a>
+              <a class="level-item" aria-label="like">
+                <span class="icon is-small">
+                <i class="fas fa-heart" aria-hidden="true"></i>
+              </span>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </article>
     </div>
   </section>
 </template>
 
 <script>
-
+  
 </script>
 
 <style>
+  
 </style>
