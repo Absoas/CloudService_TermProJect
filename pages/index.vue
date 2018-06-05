71991f540f73c62a8e60b42731784e6bf8c627e3
YbCloud <abso8787@naver.com>
2018-06-05 12:07:41 +0900

    0605

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,20 @@
     <hr>
   
     <div class="box">
+      <br>
+      <br>
+      <br>
+      <br>
+      
       <h1 class="title is-4">안녕하세요 김용빈을 소개하는 웹사이트입니다.</h1>
+       <br>
+        <br>
+         <br>
+          <br>
       <article class="media">
         <div class="media-left">
-          <figure class="image is-128x128">
-            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+          <figure class="image is-512x512">
+            <img src="~static/dogfoot.jpg" alt="Image">
           </figure>
         </div>
         <div class="media-content">
